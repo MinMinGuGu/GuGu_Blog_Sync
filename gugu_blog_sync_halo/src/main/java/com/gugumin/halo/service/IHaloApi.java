e5dfbf642ae44f74c2f7c568e1dc3cb3e7b84369
MinMin <newmailbyminmin@gmail.com>
2023-03-09 10:09:18 +0800

    添加默认配置信息

--- gugu_blog_sync_halo/src/main/java/com/gugumin/halo/service/IHaloApi.java
+++ gugu_blog_sync_halo/src/main/java/com/gugumin/halo/service/IHaloApi.java
@@ -10,10 +10,10 @@ import com.gugumin.halo.pojo.request.Account;
  * @date 2023/03/08
  */
 public interface IHaloApi {
-    @Post("${url}/admin/login")
+    @Post("${url}/api/admin/login")
     String login(@JSONBody Account account);
-    @Get(url = "${url}/admin/posts", headers = {"ADMIN-Authorization: ${token}"}, data = {"size=65535", "status=PUBLISHED"})
+    @Get(url = "${url}/api/admin/posts", headers = {"ADMIN-Authorization: ${token}"}, data = {"size=65535", "status=PUBLISHED"})
     String posts();
-    @Get(url = "${url}/admin/posts/${postId}", headers = {"ADMIN-Authorization: ${token}"})
+    @Get(url = "${url}/api/admin/posts/${postId}", headers = {"ADMIN-Authorization: ${token}"})
     String getPost();
 }
